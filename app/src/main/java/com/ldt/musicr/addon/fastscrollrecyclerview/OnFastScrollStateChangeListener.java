a772474f7ab8be80e648f5900eae99ae43fe9f63
Iswandi <andi.jr103@gmail.com>
2022-12-01 17:18:56 +0700

    Initial Commit

---
+++ app/src/main/java/com/ldt/musicr/addon/fastscrollrecyclerview/OnFastScrollStateChangeListener.java
@@ -0,0 +1,14 @@
+package com.ldt.musicr.addon.fastscrollrecyclerview;
+//Teuku Khairul Umam
+public interface OnFastScrollStateChangeListener {
+
+    /**
+     * Called when fast scrolling begins
+     */
+    void onFastScrollStart();
+
+    /**
+     * Called when fast scrolling ends
+     */
+    void onFastScrollStop();
+}
---
+++ app/src/test/java/com/ldt/trung/musicplayer/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.ldt.trung.musicplayer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//Teuku Khairul Umam
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
